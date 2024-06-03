691702989628532e1f353ef00d7efe71e88398a0
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-06-03 11:17:18 +0200

    speichern der Zeitstempel 'a' statt überschreiben 'w'

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ bestellungen['Freigabe'].replace({'Ja': 1, 'Nein': 0}, inplace=True)
 bestellungen.to_csv(r'L:\Q\Vorabmuster.csv', sep=';', index=False)
 
 #Log File
-with open(r'S:\EMEA\Kontrollabfragen\Vorabmuster.txt', 'w') as f:
-    f.write(f'Last MPS copied at: {dt_c}')
+with open(r'S:\EMEA\Kontrollabfragen\Vorabmuster.txt', 'a') as f:
+    f.write(f'\nLast MPS copied at: {dt_c}')
     f.close()
 
 
-messagebox.showinfo('Update Erfolgreich!', f'Das Vorabmuster-Update mit letzter Änderung vom {dt_c} wurde erfolgreich durchgeführt.')
+#messagebox.showinfo('Update Erfolgreich!', f'Das Vorabmuster-Update mit letzter Änderung vom {dt_c} wurde erfolgreich durchgeführt.')
