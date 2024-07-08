5be5c206879154ddcb4e88afc8f2b19ece4ca4ea
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-07-08 09:59:55 +0200

    variabler user, ausführungs log und änderungs log

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from datetime import datetime
 import os
 
 warnings.filterwarnings('ignore')
-
-link = r'C:\Users\ChristophRazek\Emea\06_Qualitymanagement - Dokumente\01_QS\04_MPS\Vorabmuster.xlsx'
+user = os.getlogin()
+link = rf'C:\Users\{user}\Emea\06_Qualitymanagement - Dokumente\01_QS\04_MPS\Vorabmuster.xlsx'
 
 #Zeitstempel Letzte Änderung!
 c_time = os.path.getmtime(link)
 dt_c = datetime.fromtimestamp(c_time)
+now = datetime.now()
 
 #Kopieren von MPS File zu Laufwerk
 bestellungen = pd.read_excel(link)
@@ -25,7 +26,7 @@ bestellungen.to_csv(r'L:\Q\Vorabmuster.csv', sep=';', index=False)
 
 #Log File
 with open(r'S:\EMEA\Kontrollabfragen\Vorabmuster.txt', 'a') as f:
-    f.write(f'\nLast MPS copied at: {dt_c}')
+    f.write(f'\nLast MPS copied at: {now}, last change: {dt_c}')
     f.close()
 
 
